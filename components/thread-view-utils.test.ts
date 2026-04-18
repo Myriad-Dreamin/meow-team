02f17cb574f867f0703cde73c569d6cf3739d7b1
Myriad-Dreamin <camiyoru@gmail.com>
2026-04-19 07:30:53 +0800

    fix: lint

--- components/thread-view-utils.test.ts
+++ components/thread-view-utils.test.ts
@@ -6,6 +6,7 @@ import {
   formatAssignmentStatusLabel,
   formatPoolSlot,
   formatCommitHash,
+  getLaneApprovalAction,
   getLaneApprovalActions,
   groupThreadLogEntries,
   getLaneBranchDisplay,
